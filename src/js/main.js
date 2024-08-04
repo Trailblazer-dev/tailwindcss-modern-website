d959e002be6bf8f33aebd2b3230917cd801e6104
Trailblazer-dev <richvictor830@gmail.com>
2024-08-04 20:37:33 +0300

    building

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,19 @@ function linkAction() {
   menu.classList.add("hidden");
 }
 navLink.forEach((current)=>{current.addEventListener("click", linkAction);})
+
+
+// testimonials
+const userTexts = document.getElementsByClassName('.user-text');
+const userPics = document.getElementsByClassName('.user-pic');
+
+function showReview(){
+  for(userPic of userPics){
+    userPic.classList.remove('active-pic');
+  }
+  for(userText of userTexts){
+    userText.classList.remove('active-text');
+  }
+  let i = Array.from(userPics).indexOf(event.target);
+  userPics[i].classList.add('active-pic')
+}
